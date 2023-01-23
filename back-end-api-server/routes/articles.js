5c303f34bc6b57e574e636c0639fc4c021f56141
speedywpm <ernestaszimus@gmail.com>
2023-01-23 16:25:43 +0000

    updated updateArticle and deleteArticle functions

--- back-end-api-server/routes/articles.js
+++ back-end-api-server/routes/articles.js
@@ -10,7 +10,7 @@ const router = Router({prefix: '/api/v1/articles'});
 //temp def some random articles in an array
 //later change implement a database
 
-let articles = [
+const articles = [
     {title:'hello article', fullText:'some text here to fill the body'},
     {title:'another article', fullText:'again here is some text here to fill'},
     {title:'coventry university', fullText:'some news about coventry university'}
@@ -24,10 +24,10 @@ let articles = [
 router.get('/', getAll)
 router.post('/', bodyParser(), createArticle);
 router.post('/', bodyParser(), updateArticle);
-
+router.post('/', bodyParser(), deleteArticle);
 router.get('/:id([0-9]{1,})', getById);
-router.get('/:id([0-9]{1,})', updateArticle);
-router.get('/:id([0-9]{1,})', deleteArticle);
+router.get('/update/:id([0-9]{1,})', updateArticle);
+router.get('/delete/:id([0-9]{1,})', deleteArticle);
 
 //define handler functions used above
 
@@ -68,20 +68,34 @@ function createArticle(cnx, next)
     cnx.body = newArticle;
 }
 
-function updateArticle(cnx, next)
+function updateArticle(cnx, articles ,next)
 {
     //edit an article
+    // i give an id already which article to update
+    // 
     //let {title, fullText} = cnx.request.body;
-    //let updArticle = {title: 'updated', fullText: 'really updated'};
-
-    //cnx.status = 201;
-    //articles.push(updArticle);
+    let updArticle = {title: 'updated', fullText: 'really updated'}; //supposedly whats to update
+    let articleID = cnx.originalUrl
+    const lastItem = articleID.substring(articleID.lastIndexOf('/') + 1)
+    const numID = Number(lastItem);
+    cnx.status = 201;
+    console.log(numID); // i get the id
+    articles[numID-1] = updArticle; //-1 because arrays are counted from 0
+    //gets an article id, therefore gives it to an array and the text to update
+    //need to create something to see updates itself.
+    //cnx.body = articles;
 }
 
-function deleteArticle(cnx, next)
+function deleteArticle(cnx, articles, next)
 {
     //delete an article
-
+    // idea is to use a splice, as it is able to remove an element from an array and reindex it
+    let articleID = cnx.originalUrl
+    const lastItem = articleID.substring(articleID.lastIndexOf('/') + 1)
+    const numID = Number(lastItem);
+    console.log(numID); // i get the id
+    delete articles[numID-1];
+    cnx.status = 201;
 }
 
 // define the exported object when required from other scripts
